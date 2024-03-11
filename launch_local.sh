ee9841d899e8dd6da972bfe5e00285bcbc036120
Jonathan Schwarz <schwarzjn@gmail.com>
2024-03-11 09:46:57 -0400

    updated configs

--- launch_local.sh
+++ launch_local.sh
@@ -1,10 +1,23 @@
 DATA_SPLIT='complex_disease'
 
 python3 finetune.py --model='dkl' \
-     --dataset='txgnn_did' \
-     --n_epochs=30 \
-     --batch_size=128 \
-     --learning_rate=0.01 \
-     --scheduler_type='cosine_decay_with_warmup' \
-     --weight_decay=0.01 \
-     --wandb_track=True
+                    --dataset='txgnn_did' \
+                    --n_epochs=30 \
+                    --batch_size=128 \
+                    --learning_rate=0.01 \
+                    --scheduler_type='valid_plateau' \
+                    --valid_every=25 \
+                    --wandb_track=True
+
+#python3 finetune.py --model='dkl_llama2_7b' \
+#                    --use_fromage=True \
+#                    --n_layers=1 \
+#                    --finetune_type='lora' \
+#                    --dataset='txgnn_did' \
+#                    --n_epochs=30 \
+#                    --batch_size=24 \
+#                    --learning_rate=0.001 \
+#                    --scheduler_type='cosine_decay_with_warmup' \
+#                    --valid_every=250 \
+#                    --weight_decay=0.01 \
+#                    --wandb_track=True

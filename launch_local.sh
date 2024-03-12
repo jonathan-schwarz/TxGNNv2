be23b24668d0dbe8e92c74670ef54148e3446571
Jonathan Schwarz <schwarzjn@gmail.com>
2024-03-12 13:14:09 -0400

    Adding config files for reproducability

--- launch_local.sh
+++ launch_local.sh
@@ -1,23 +1,7 @@
 DATA_SPLIT='complex_disease'
 
-python3 finetune.py --model='dkl' \
-                    --dataset='txgnn_did' \
-                    --n_epochs=30 \
-                    --batch_size=128 \
-                    --learning_rate=0.01 \
-                    --scheduler_type='valid_plateau' \
-                    --valid_every=25 \
-                    --wandb_track=True
+# DKL only
+# python3 finetune.py --flagfile=configs/dkl/txgnn_did_default.cfg
 
-#python3 finetune.py --model='dkl_llama2_7b' \
-#                    --use_fromage=True \
-#                    --n_layers=1 \
-#                    --finetune_type='lora' \
-#                    --dataset='txgnn_did' \
-#                    --n_epochs=30 \
-#                    --batch_size=24 \
-#                    --learning_rate=0.001 \
-#                    --scheduler_type='cosine_decay_with_warmup' \
-#                    --valid_every=250 \
-#                    --weight_decay=0.01 \
-#                    --wandb_track=True
+# DKL + LLM
+python3 finetune.py --flagfile=configs/dkl_llama2_7b/txgnn_did_default.cfg

808a0ed529e9b574c9be7356d120f8c300527344
Jonathan Schwarz <schwarzjn@gmail.com>
2024-03-12 13:33:49 -0400

    Adding Mixtral as an option

--- finetune_models/llm_models.py
+++ finetune_models/llm_models.py
@@ -1,4 +1,4 @@
-"""LLM Models."""
+'''LLM Models.'''
 import evaluate
 import numpy as np
 import torch
@@ -17,14 +17,14 @@ from transformers import (
     BitsAndBytesConfig,
     pipeline,
 )
-from .dkl_llama import LlamaForDKLSequenceClassification
+from .custom_llama import LlamaForCustomSequenceClassification
 from .models import FeatureExtractor
 from peft import LoraConfig
 
-accuracy = evaluate.load("accuracy")
+accuracy = evaluate.load('accuracy')
 
-ID2LABEL = {0: "NEGATIVE", 1: "POSITIVE"}
-LABEL2ID = {"NEGATIVE": 0, "POSITIVE": 1}
+ID2LABEL = {0: 'NEGATIVE', 1: 'POSITIVE'}
+LABEL2ID = {'NEGATIVE': 0, 'POSITIVE': 1}
 
 
 CACHE_PATH = '/n/holyscratch01/mzitnik_lab/jschwarz/cache/'
@@ -36,7 +36,7 @@ def _get_gpu_utilization():
     nvmlInit()
     handle = nvmlDeviceGetHandleByIndex(0)
     info = nvmlDeviceGetMemoryInfo(handle)
-    return f"GPU memory occupied: {info.used//1024**2} MB."
+    return f'GPU memory occupied: {info.used//1024**2} MB.'
 
 
 def compute_metrics(eval_pred):
@@ -60,100 +60,109 @@ def get_tokenizer(model_type):
     if 'distilbert' in model_type:
         tokenizer = AutoTokenizer.from_pretrained(
             'distilbert-base-uncased', cache_dir=CACHE_PATH)
-        tokenizer = AutoTokenizer.from_pretrained('distilbert-base-uncased')
+    elif  'mixtral' in model_type:
+        tokenizer = AutoTokenizer.from_pretrained(
+            'mistralai/Mixtral-8x7B-v0.1', cache_dir=CACHE_PATH)
+        # TODO(schwarzjn): Double check
+        tokenizer.pad_token = tokenizer.eos_token
+        tokenizer.padding_side = 'right' # Fix weird overflow issue with fp16 training
     elif  'llama2_7b' in model_type:
         tokenizer = AutoTokenizer.from_pretrained(
             'NousResearch/Llama-2-7b-hf', trust_remote_code=True, cache_dir=CACHE_PATH)
+        # TODO(schwarzjn): Double check
         tokenizer.pad_token = tokenizer.eos_token
-        tokenizer.padding_side = "right" # Fix weird overflow issue with fp16 training
+        tokenizer.padding_side = 'right' # Fix weird overflow issue with fp16 training
     elif  'llama2_13b'in model_type:
         tokenizer = AutoTokenizer.from_pretrained(
             'NousResearch/Llama-2-13b-hf', trust_remote_code=True, cache_dir=CACHE_PATH)
         tokenizer.pad_token = tokenizer.eos_token
-        tokenizer.padding_side = "right" # Fix weird overflow issue with fp16 training
+        tokenizer.padding_side = 'right' # Fix weird overflow issue with fp16 training
 
     return tokenizer
 
 
-def get_llm(model_type, task_type, tokenizer):
+def get_llm(model_type, task_type, tokenizer, use_4bit=True):
     # Load the entire model on the GPU 0
-    device_map = {"": 0}
+    # TODO(schwarzjn): Fix for multi-GPU jobs
+    device_map = {'': 0}
+
+    # Check GPU compatibility with bfloat16
     use_bf16 = False
+
+    # Check if we can use bfloat16
+    compute_dtype = getattr(torch, 'float16')
+    if compute_dtype == torch.float16 and use_4bit:
+        major, _ = torch.cuda.get_device_capability()
+        if major >= 8:
+            print('=' * 80)
+            print('Your GPU supports bfloat16: accelerating training with use_bf16=True')
+            print('=' * 80)
+            use_bf16 = True
+
+    bnb_config = BitsAndBytesConfig(
+        load_in_4bit=use_4bit,
+        bnb_4bit_quant_type='nf4',
+        bnb_4bit_compute_dtype=compute_dtype,
+        bnb_4bit_use_double_quant=False,
+    )
+
     if 'distilbert' == model_type:
+        # No quantization necessary for DistilBert (small model)
         model = AutoModelForSequenceClassification.from_pretrained(
-            "distilbert-base-uncased",
+            'distilbert-base-uncased',
             device_map=device_map,
             num_labels=2,
             id2label=ID2LABEL,
             label2id=LABEL2ID,
+            cache_dir=CACHE_PATH,
         )
-    elif 'llama2' in model_type:
-        # Load tokenizer and model with QLoRA configuration
-        compute_dtype = getattr(torch, "float16")
-        bf16 = False
-        use_4bit = True
-        # Check GPU compatibility with bfloat16
-        if compute_dtype == torch.float16 and use_4bit:
-            major, _ = torch.cuda.get_device_capability()
-            if major >= 8:
-                print("=" * 80)
-                print("Your GPU supports bfloat16: accelerating training with bf16=True")
-                print("=" * 80)
-                bf16 = True
+    elif 'mixtral' in model_type:
+        model = AutoModelForSequenceClassification.from_pretrained(
+            'mistralai/Mixtral-8x7B-v0.1',
+            quantization_config=bnb_config,
+            device_map=device_map,
+            num_labels=2,
+            id2label=ID2LABEL,
+            label2id=LABEL2ID,
+            cache_dir=CACHE_PATH,
+        )
 
-        bnb_config = BitsAndBytesConfig(
-            load_in_4bit=use_4bit,
-            bnb_4bit_quant_type="nf4",
-            bnb_4bit_compute_dtype=compute_dtype,
-            bnb_4bit_use_double_quant=False,
-        )
+        model.config.use_cache = True
+    elif 'llama2' in model_type:
 
         if'llama2_7b' in model_type:
-            model_name = "NousResearch/Llama-2-7b-hf"
+            model_name = 'NousResearch/Llama-2-7b-hf'
         elif'llama2_13b' in model_type:
-            model_name = "NousResearch/Llama-2-13b-hf"
+            model_name = 'NousResearch/Llama-2-13b-hf'
 
-        if 'SEQ_CLS' == task_type:
-            if 'dkl' in model_type:
-                model = LlamaForDKLSequenceClassification.from_pretrained(
-                    model_name,
-                    quantization_config=bnb_config,
-                    device_map=device_map,
-                    cache_dir=CACHE_PATH,
-                    num_labels=2,
-                    id2label=ID2LABEL,
-                    label2id=LABEL2ID,
-                )
-            else:
-                model = AutoModelForSequenceClassification.from_pretrained(
-                    model_name,
-                    quantization_config=bnb_config,
-                    device_map=device_map,
-                    num_labels=2,
-                    id2label=ID2LABEL,
-                    label2id=LABEL2ID,
-                    cache_dir=CACHE_PATH,
-                )
-        else:
-            model = AutoModelForCausalLM.from_pretrained(
+        if '_llama2_7b' in model_type:
+            # Custom version
+            model = LlamaForCustomSequenceClassification.from_pretrained(
                 model_name,
                 quantization_config=bnb_config,
-                device_map=device_map
+                device_map=device_map,
+                cache_dir=CACHE_PATH,
+                num_labels=2,
+                id2label=ID2LABEL,
+                label2id=LABEL2ID,
             )
-
-            # Make predictions
-            pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer, max_length=200)
-            prompt = txgnn['test'][0]['text']
-            result = pipe(f"<s>[INST] {prompt} [/INST]")
-            print(result[0]['generated_text'])
+        else:
+            model = AutoModelForSequenceClassification.from_pretrained(
+                model_name,
+                quantization_config=bnb_config,
+                device_map=device_map,
+                num_labels=2,
+                id2label=ID2LABEL,
+                label2id=LABEL2ID,
+                cache_dir=CACHE_PATH,
+            )
 
         model.config.use_cache = False
         model.config.pretraining_tp = 1
-        use_bf16 = True
 
-    print("=" * 80)
-    print("Model loaded. " + _get_gpu_utilization())
-    print("=" * 80)
+    print('=' * 80)
+    print('Model loaded. ' + _get_gpu_utilization())
+    print('=' * 80)
 
     return model, use_bf16
 
@@ -167,13 +176,12 @@ def get_peft(model, task_type, finetune_type, lora_apply_everywhere, use_final_l
             'lora_alpha': 16,
             'lora_dropout': 0.1,
             'r': 64,
-            'bias': "none",
-            #  'modules_to_save': ["score"],
+            'bias': 'none',
         }
         if use_final_layer:
-            kwargs['modules_to_save'] = "score"
+            kwargs['modules_to_save'] = 'score'
         if not lora_apply_everywhere:
-            kwargs['target_modules'] = ["q_proj", "k_proj", "v_proj"]  # "out_proj", "fc_in", "fc_out", "wte"]
+            kwargs['target_modules'] = ['q_proj', 'k_proj', 'v_proj']  # 'out_proj', 'fc_in', 'fc_out', 'wte']
 
         peft_config = LoraConfig(**kwargs)
         optim = 'paged_adamw_32bit'
